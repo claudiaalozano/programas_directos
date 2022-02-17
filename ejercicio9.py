6ec08d5c490268e611b939fdf4449e670125a44b
claudiaalozano <clau.lozano.perez@gmail.com>
2022-02-17 20:15:02 +0100

    cambio fallos en el programa

--- ejercicio9.py
+++ ejercicio9.py
@@ -3,8 +3,8 @@ numero1= numeros.append(float(input("Introduzca el primer número: ")))
 numero2= numeros.append(float(input("Introduzca el segundo número: ")))
 numero3= numeros.append(float(input("Introduzca el tercer número: ")))
 suma= 0
-for i in range(numeros):
-    suma = i + numeros
+for i in numeros:
+    suma = i + suma
 
 suma = suma / 3 
 print ("La media aritmética es: " , suma )
