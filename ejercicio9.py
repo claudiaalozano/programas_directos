fdc4275eda7ff1ea49c0aa12af86887bed9dc4e2
claudiaalozano <clau.lozano.perez@gmail.com>
2022-02-17 20:05:41 +0100

    creo un bucle for para sumar los tres numeros

--- ejercicio9.py
+++ ejercicio9.py
@@ -2,5 +2,7 @@ numeros=[]
 numero1= numeros.append(float(input("Introduzca el primer número: ")))
 numero2= numeros.append(float(input("Introduzca el segundo número: ")))
 numero3= numeros.append(float(input("Introduzca el tercer número: ")))
-
-coeficiente1= float()
+suma= 0
+for i in range(numeros):
+    suma = i + numeros
+    
