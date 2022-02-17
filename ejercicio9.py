c5cb9573180233ed9f287af58ace997fb4cdaadf
claudiaalozano <clau.lozano.perez@gmail.com>
2022-02-17 19:58:49 +0100

    creo las variables para los tres números junto con la lista en la que van a quedar almacenados

---
+++ ejercicio9.py
@@ -0,0 +1,6 @@
+numeros=[]
+numero1= numeros.append(float(input("Introduzca el primer número: ")))
+numero2= numeros.append(float(input("Introduzca el segundo número: ")))
+numero3= numeros.append(float(input("Introduzca el tercer número: ")))
+
+coeficiente1= float()
