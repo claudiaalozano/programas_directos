fcb48458b496a6d6832052548e77b755b8ad3ab2
claudiaalozano <clau.lozano.perez@gmail.com>
2022-02-18 14:28:05 +0100

    commit

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,2 +1,8 @@
 precio = float(input("Introduzca el precio del producto: "))
- 
+iva = (precio * 21 ) / 100 
+precio_iva = precio + iva
+vivienda = input("¿Que tipo de vivienda quiere calcular para el ibi? (casa/terreno):")
+if vivienda == "casa":
+    print("Usted debe pagar un total de (...) ")
+if vivienda == "terreno":
+    print("Usted debe de pagar un total de (...) ")
