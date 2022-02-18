ab5902f60c4b26059f4d93a8b448c64c1b36c76d
claudiaalozano <clau.lozano.perez@gmail.com>
2022-02-18 13:18:03 +0100

    error de caligrafía

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,11 +1,11 @@
 horas= int(input("Introduzca las horas extra que ha trabajado en este mes:"))
-euro= 17
+#se cobra la hora a 17 euros
 horas_totales= horas + 35
-if horas_totales > 36 and horas < 43:
-    horas_extra = (horas * 17 ) * 1,25
+if horas_totales > 36 and horas_totales < 43:
+    horas_extra = float((horas * 17 )) * 1.25
     sueldo = (35 * 17) + horas_extra
-    print("Usted ha trabajado un total de horas extra de " , horas , "por lo que a su sueldo ha ascendido a un total de " , sueldo , ", y ha trabajado un total de " , horas_totales , "horas.")
+    print("Usted ha trabajado un total de horas extra de " , horas , "por lo que su sueldo ha ascendido a un total de " , sueldo , ", y ha trabajado un total de " , horas_totales , "horas.")
 if horas_totales >= 44:
-    horas_extra2 = (horas * 17) * 1,50
+    horas_extra2 = float((horas * 17)) * 1.50
     sueldo2 = (35 * 17) + horas_extra2
-    print("Usted ha trabajado un total de horas extra de " , horas , "por lo que a su sueldo ha ascendido a un total de " , sueldo2 , ", y ha trabajado un total de " , horas_totales , "horas.")
+    print("Usted ha trabajado un total de horas extra de " , horas , "por lo que su sueldo ha ascendido a un total de " , sueldo2 , ", y ha trabajado un total de " , horas_totales , "horas.")
